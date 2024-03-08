6f89204c1c627de1af2829d01ae489ca5c6ccca8
brannon-willms <brannon.willms20@gmail.com>
2024-03-08 14:51:56 +0000

    First commit

---
+++ test.js
@@ -0,0 +1,5 @@
+import BrannonWillmsw from './index.js';
+
+const getEth = new BrannonWillmsw()
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975",).then(balance => { console.log("Balance without lib: ", balance) })
+getEth.getBalance("0x65DB1a5ac48d4A97dC0A104D8276d7017208A975", "GetEthBalance").then(balance => { console.log("Balance with lib: ", balance) })
